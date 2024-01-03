5952bbea2e5b13834ac837fd4051f0123a1a13ee
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2024-01-03 14:19:58 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as fh:
+#     description = fh.read()
+
+setup(
+    name="utils",
+    version="0.0.1",
+    description="a pip-installable package example",
+    # long_description=description,
+    # long_description_content_type="text/markdown",
+    # license='MIT',
+    # packages=['utils'],
+    packages=find_packages(),
+    author="Nikola Bozhinov",
+    author_email="n.bozhinov@klett.bg",
+    # keywords=['example'],
+    # url='https://github.com/bgse/hellostackoverflow',
+    # python_requires=">=3.8",
+    # install_requires=[],
+)
